df5ee09173efeb23309e2fc6779e7b1fc7c90489
湫曗 <autumnsilverleaves@gmail.com>
2021-06-08 21:09:05 +0800

    use element

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./view/router/router";
+
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
+
 const app = createApp(App);
 app.use(router);
+app.use(ElementPlus, { size: "small" });
 app.mount("#app");
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -2,6 +2,14 @@
     <div>{{ user.userInfo.value.name }}</div>
     <div>{{ user.userInfo.value.hobbies }}</div>
     <div>{{ config.config }}</div>
+    <el-row>
+        <el-button plain>朴素按钮</el-button>
+        <el-button type="primary" plain>主要按钮</el-button>
+        <el-button type="success" plain>成功按钮</el-button>
+        <el-button type="info" plain>信息按钮</el-button>
+        <el-button type="warning" plain>警告按钮</el-button>
+        <el-button type="danger" plain>危险按钮</el-button>
+    </el-row>
 </template>
 
 <script lang="ts">
